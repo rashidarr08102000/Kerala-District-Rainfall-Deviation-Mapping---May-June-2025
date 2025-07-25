93ca177ed8045421c0665c5647a952e681a6b500
rashidarr08102000 <159603507+rashidarr08102000@users.noreply.github.com>
2025-07-25 12:57:27 +0530

    Add files via upload

---
+++ rainfall_deviation_map.py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+gdf = gpd.read_file("Export_Output.shp")
+gdf['District'] = gdf['NAME_2'].str.strip().str.upper()
+
+df_actual = pd.read_excel("rainfall_kerala_actual.xlsx")
+df_normal = pd.read_excel("rainfall_kerala_normal.xlsx")
+
+# Standardize district names
+df_actual['District'] = df_actual['District'].str.strip().str.upper()
+df_normal['District'] = df_normal['District'].str.strip().str.upper()
+
+df = df_actual.merge(df_normal, on='District', suffixes=('_actual', '_normal'))
+
+# Calculating deviation
+df['May_deviation'] = df['May_actual'] - df['May_normal']
+
+# Merge with shapefile
+merged = gdf.merge(df, on='District', how='left')
+
+# Plot the map
+fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+
+merged.plot(
+    column='May_deviation',
+    cmap='Blues',  # Red = below normal, Blue = above normal
+    legend=True,
+    edgecolor='black',
+    linewidth=0.7,
+    ax=ax,
+    legend_kwds={
+        'shrink': 0.6,
+        'label': 'Rainfall Deviation (mm)'
+    },
+    vmin=400, vmax=1100 
+)
+
+ax.set_title("Kerala Rainfall Deviation: May 2025", fontsize=16)
+ax.axis('off')
+plt.tight_layout()
+plt.savefig("kerala_rainfall_deviation_may_2025.png", dpi=300)
+plt.show()
---
+++ rainfall_normal_map .py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from google.colab import files
+uploaded = files.upload()
+
+gdf = gpd.read_file("Export_Output.shp")
+
+gdf['District'] = gdf['NAME_2'].str.strip().str.upper()
+
+# The data
+df = pd.read_excel("rainfall_kerala_normal.xlsx")
+df['District'] = df['District'].str.strip().str.upper()
+
+merged = gdf.merge(df, on='District', how='left')
+
+# select the month
+rainfall_column = 'June'
+
+# Plot the map
+fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+
+merged.plot(
+    column=rainfall_column,
+    cmap='Blues',
+    legend=True,
+    edgecolor='black',
+    linewidth=0.6,
+    ax=ax,
+    legend_kwds={
+        'shrink': 0.6,
+        'label': 'Normal Rainfall (mm)',
+    },
+    vmin=400, vmax=1100
+)
+
+ax.set_title(
+    f"Kerala District-wise Normal Rainfall: {rainfall_column} 2025",
+    fontsize=16)
+ax.axis('off')
+plt.tight_layout()
+
+plt.savefig(f"kerala_normal_rainfall_{rainfall_column.lower()}_2025.png", dpi=300)
+plt.show()
